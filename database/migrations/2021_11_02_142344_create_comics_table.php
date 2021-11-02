98bfbef5af8b78bee3e6cc0207030f7485eee830
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-02 15:29:22 +0100

    migration done

--- database/migrations/2021_11_02_142344_create_comics_table.php
+++ database/migrations/2021_11_02_142344_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->float('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
